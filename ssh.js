429b7018a886f9bbe916625d3d6227de2726d634
DikshitaDomadiya <dikshita@dvijinfotech.com>
2023-08-25 12:55:15 +0530

    first commit

---
+++ ssh.js
@@ -0,0 +1,21 @@
+// $ git config user.name
+
+// $ git config user.email
+// dikshita.dvijinfotech@gmail.com
+
+// $ git config --global user.email "dikshita@dvijinfotech.com"  //for change the email
+
+// $ ssh-keygen -t ed25519 -C "dikshita.dvijinfotech@gmail.com"    // create key
+
+// $  cat ~/.ssh/id_ed25519.pub     // display key
+
+//$ git clone git@github.com:dikshita8080/type-script.git   // clone ssh
+
+
+// $ git init
+//$ cat ssh.js 
+ 
+//$ git add .
+//$ git commit -m "first commit"
+//$ git remote add origin git@github.com:dikshita8080/type-script.git
+//$ git push origin master
